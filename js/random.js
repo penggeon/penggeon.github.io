38208273e13f0c4ed2a7c21e1e597d37a6ae2371
polar-bei <xiaoxinpro030703@outlook.com>
2024-02-21 00:59:04 +0800

    feat: add some content in about page

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/3906.html","posts/48839.html","posts/7474.html","posts/20930.html","posts/27859.html","posts/34498.html","posts/6413.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/3906.html","posts/48839.html","posts/7474.html","posts/20930.html","posts/27859.html","posts/6413.html","posts/34498.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
