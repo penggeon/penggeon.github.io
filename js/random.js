bf3ec9a79f7e4905a149e3f5a64ba74a46466b45
penggeon <penggeon@outlook.com>
2024-05-18 01:32:45 +0800

    fix: change blog to jianjie version

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/48839.html","posts/3906.html","posts/7474.html","posts/af59e567.html","posts/20930.html","posts/27859.html","posts/34498.html","posts/6413.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/3906.html","posts/7474.html","posts/af59e567.html","posts/48839.html","posts/20930.html","posts/27859.html","posts/34498.html","posts/6413.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
